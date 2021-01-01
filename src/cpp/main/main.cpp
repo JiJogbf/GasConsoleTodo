dc2cfb7fe8526628ad1ffa1f963640b8fea9e82a
JiJogbf <alex.gas1995@ya.ru>
2021-01-01 14:56:35 +0300

    Fixing cmakelists buildscript x2

--- src/cpp/main/main.cpp
+++ src/cpp/main/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+struct Drawable{
+    virtual ~Drawable() = 0;
+    virtual void draw() = 0;
+};
+
+Drawable::~Drawable(){}
+
 int main(){
     std::cout << "Console Todo app" << std::endl;
     return 0;
